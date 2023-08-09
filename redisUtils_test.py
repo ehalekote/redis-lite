b1f90732b468a20f5d0daed9e1fd5139a7983e98
Eshan <>
2023-08-09 07:04:18 +0530

    add deserialize Error tests

--- redisUtils_test.py
+++ redisUtils_test.py
@@ -7,7 +7,12 @@ from redisUtils import *
         # Simple String test cases
          ("+PING", None),
          ("+OK\r\n", SimpleString('OK')),
-         ("+OK\r\n+Next", SimpleString('OK'))
+         ("+OK\r\n+Next", SimpleString('OK')),
+
+         # Errors test cases
+         ("-Error message", None),
+         ("-Error message\r\n", Error('Error message')),
+         ("-Error message\r\n+Next", Error('Error message'))
     ]
 )
 def test_deserialize(buffer, expected):
