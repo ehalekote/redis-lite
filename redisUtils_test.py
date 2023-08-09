27bf8cf4c34e71b878f8595c577f454a81bcfd78
Eshan <>
2023-08-09 11:46:45 +0530

    add deserialize BulkString tests

--- redisUtils_test.py
+++ redisUtils_test.py
@@ -17,7 +17,14 @@ from redisUtils import *
          # Integer test cases
          (":10", None),
          (":10\r\n", Integer(10)),
-         (":10\r\n+Next", Integer(10))
+         (":10\r\n+Next", Integer(10)),
+
+         # Bulk String test cases
+         ("$-1\r\n", None),
+         ("$5\r\nhello", None),
+         ("$0\r\n\r\n", BulkString("")),
+         ("$5\r\nhello\r\n", BulkString("hello")),
+         ("$5\r\nhello\r\n+Next", BulkString("hello")),
     ]
 )
 def test_deserialize(buffer, expected):
