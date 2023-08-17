8fd0d4908aca18c1a0907aa36198271c79a3687a
Eshan <>
2023-08-17 11:24:48 +0530

    support GET and SET

--- app/server.py
+++ app/server.py
@@ -2,15 +2,29 @@ import socket
 
 from redisUtils import deserialize, serialize, SimpleString, BulkString, Error
 
+memory = {}
+
 
 def handleRequest(respArray, length):
     arr = respArray.data
     cmdBulkString = arr[0]
+
     if cmdBulkString.data == "PING":
         return (SimpleString("PONG"),)
-    if cmdBulkString.data == "ECHO" and len(arr) > 1:
+    elif cmdBulkString.data == "ECHO" and len(arr) > 1:
         payload = arr[1].data
         return (BulkString(payload), len(payload))
+    elif cmdBulkString.data == "SET" and len(arr) > 2:
+        setKey = arr[1].data
+        setValue = arr[2].data
+        memory[setKey] = setValue
+        return (SimpleString("OK"),)
+    elif cmdBulkString.data == "GET" and len(arr) > 1:
+        targetKey = arr[1].data
+        if targetKey not in memory:
+            return (BulkString(None), 5)
+        targetValue = memory[targetKey]
+        return (BulkString(targetValue), len(targetValue))
 
     return (Error("Invalid or unsupported command"),)
 
