3e54e0d65d2971bc5c7c80f88cdc9672caee899b
Eshan <>
2023-08-17 09:55:04 +0530

    make server continuously listen

--- app/server.py
+++ app/server.py
@@ -21,18 +21,19 @@ PORT = 6379
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
+
+    while True:
+        conn, addr = s.accept()
         print(f"Connected by {addr}")
-        while True:
-            dataBytes = conn.recv(1024)
-            if not dataBytes:
-                break
-            dataTuple = deserialize(dataBytes.decode("utf-8"))
-            respArray = dataTuple[0]
-            length = dataTuple[1]
 
-            res = handleRequest(respArray, length)
-            resSerialized = serialize(res)
+        dataBytes = conn.recv(1024)
 
-            conn.sendall(resSerialized.encode())
+        dataTuple = deserialize(dataBytes.decode("utf-8"))
+        respArray = dataTuple[0]
+        length = dataTuple[1]
+
+        res = handleRequest(respArray, length)
+        resSerialized = serialize(res)
+
+        conn.sendall(resSerialized.encode())
+        conn.close()
